4d88da3895f5283a5f1fa52680ded14c0a6da07c
CosminTurca <cosminturca100@gmail.com>
2026-01-13 23:23:13 +0200

    Error solved: unique tasks

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -3,11 +3,8 @@ const admin = require("firebase-admin");
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      // MODIFICARE: Folosește project_id în loc de projectId
       project_id: process.env.FIREBASE_PROJECT_ID,
-      // MODIFICARE: Folosește client_email în loc de clientEmail
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      // MODIFICARE: Folosește private_key în loc de privateKey
       private_key: process.env.FIREBASE_PRIVATE_KEY 
         ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") 
         : undefined,
